ae9aff04eee212e8d5c4b2931baf16ed8291c8bf
Sachin Kumar <you@example.com>
2025-01-08 17:00:47 +0530

    learn react component -> functional component, class based component, component compostion

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{ id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I'm h1 💪 tag"),
-        React.createElement("h2", {}, "I'm  💪 tag"),
-    ]),
-
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm h1 💪 tag"),
-        React.createElement("h2", {}, "I'm h2 💪 tag"),
-    ])
-]);
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// JSX
+// jsx => babel transpiles to react.createElement => html
+const Jsxheading = <h1 className="heading1"> React Element sachin kumar ❤️</h1>;
 
 
+
+// React component
+// Class Based component -> old way of writying component
+// Functional Component -> new way of writing component
+
+const HeadingComponent = () => {
+    return <h1>Functional react component 🙏</h1>
+};
+
+const  HeadingComponent2 = () => {
+    return (
+        <div className="container">
+            {/* here i also rendered another functional component HeadingComponent */}
+            <HeadingComponent />  
+            <h1>React Fucntional component  for multiple line of jsx</h1>
+        </div> 
+    )
+};
+
+
+
+// rendering of react element or jsx
+const jsxroot = ReactDOM.createRoot(document.getElementById("root1"));
+jsxroot.render(Jsxheading);
+
+
+
+
+// now how to render functional component;
+
+const root = ReactDOM.createRoot(document.getElementById("root2"));
+root.render(<HeadingComponent />);
+
+
+const root1 = ReactDOM.createRoot(document.getElementById("root3"));
+root1.render(<HeadingComponent2 />);
 
